84fa18e8229dd3114af44b42664ea85477611308
Juan Antonio Peregrina Peregrina <juanantonio.peregrinaperegrina@usp.ceu.es>
2025-01-29 11:50:36 +0100

    Añadido res.locals

--- snake-expressPruebas/routes/index.js
+++ snake-expressPruebas/routes/index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-// Ruta principal
-router.get('/', (req, res) => {
+// Ruta principal sin res.locals
+/*router.get('/', (req, res) => {
   res.render('index', { user: req.session.user, title: 'Snake Game', message: 'Bienvenido a Snake Game', player: 'Invitado' });
 });
 
 module.exports = router;
-                     
+*/
+
+// Ruta principal con res.locals
+router.get('/', (req, res) => {
+  res.locals.title = "Snake Game"; // Título global
+  res.locals.user = req.session.user || { username: "Invitado" }; // Usuario actual
+  res.locals.message = req.session.user 
+    ? `¡Bienvenido de nuevo, ${req.session.user.username}!`
+    : "Bienvenido a Snake Game";
+
+  res.render('index');
+});
+
+module.exports = router;
